b0f7c32092ed6e346024e58a188480ffacdbcb14
juky <julian.chamale@outlook.com>
2020-03-28 03:10:11 -0600

    Added chromium lines to run fullscreen

--- .orange-pi-kiosk/install.sh
+++ .orange-pi-kiosk/install.sh
@@ -9,4 +9,7 @@ sudo apt-get install unclutter
 sudo cp /etc/xdg/xfce4/xinitrc /etc/xdg/xfce4/xinitrc.bak
 sudo cp ./xinitrc /etc/xdg/xfce4/xinitrc
 sudo cp ./.bash_profile ~/.bash_profile
+
+sudo echo 'export CHROMIUM_FLAGS="$CHROMIUM_FLAGS --window-position=0,0 --window-size=1920,1080 --start-fullscreen --kiosk --incognito --noerrdialogs --disable-translate --no-first-run --fast --fast-start --disable-infobars --disable-features=TranslateUI --disk-cache-dir=/dev/null  --password-store=basic"' >>/etc/chromium.d/default-flags
+
 sudo reboot now
